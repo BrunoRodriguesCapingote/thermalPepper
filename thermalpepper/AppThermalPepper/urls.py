7aef6261da018fd7298e9b985303155ab6febc81
BrunoCapingot <bruno.capingote@estudante.ifgoiano.edu.br>
2026-07-11 22:10:38 -0300

    Upload de ontem

--- thermalpepper/AppThermalPepper/urls.py
+++ thermalpepper/AppThermalPepper/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('financeiro/', views.financeiro, name='financeiro'),
     path('contas/', views.contas, name='contas'),
@@ -16,4 +16,9 @@ urlpatterns = [
     path('clientes/', views.clientes, name='clientes'),
     path('perfil/', views.perfil, name='perfil'),
     path('configuracaoUsuario/', views.configuracaoUsuario, name='configuracaoUsuario'),
+    path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/',views.logout_usuario,name='logout'),
 ]
+
+
+
